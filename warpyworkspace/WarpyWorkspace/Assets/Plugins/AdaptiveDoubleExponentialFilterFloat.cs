f190b1ecd26b484084c69b5a9b1e2cd6e21a3843
rafaelkuffner <rafaelkuffner@gmail.com>
2019-03-11 17:45:37 +0000

    Added new code for optitrack, and filtering
    its a smoooooooooooooth criminal *bass line goes dudududududun*

---
+++ warpyworkspace/WarpyWorkspace/Assets/Plugins/AdaptiveDoubleExponentialFilterFloat.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class AdaptiveDoubleExponentialFilterFloat
+{
+	// as described in: msdn.microsoft.com/en-us/library/jj131429.aspx
+
+	private float deltaLow = 0.02f;
+	private float deltaHigh = 0.05f;
+	private float gainLow = 0.1f;
+	private float gainHigh = 0.15f;
+	private float trendGain = 0.1f;
+
+	public bool filtering = true;
+
+	public float DeltaLow {
+		get { return deltaLow; }
+		set { deltaLow = value; }
+	}
+
+	public float DeltaHigh {
+		get { return deltaHigh; }
+		set { deltaHigh = value; }
+	}
+
+	public float GainLow {
+		get { return gainLow; }
+		set { gainLow = Mathf.Clamp (value, 0.0f, 1.0f); }
+	}
+
+	public float GainHigh {
+		get { return gainHigh; }
+		set { gainHigh = Mathf.Clamp (value, 0.0f, 1.0f); }
+	}
+
+	public float TrendGain {
+		get { return trendGain; }
+		set { trendGain = Mathf.Clamp (value, 0.0f, 1.0f); }
+	}
+
+	private float filteredValue;
+
+	public float Value {
+		get { return filteredValue; }
+		set { filteredValue = Update (value); }
+	}
+
+	private float lastValue;
+	private float trend;
+
+	public AdaptiveDoubleExponentialFilterFloat ()
+	{
+		filteredValue = float.NaN;
+		lastValue = float.NaN;
+		trend = 0.0f;
+	}
+
+	private float Update (float newValue)
+	{
+		if (float.IsNaN (filteredValue) || !filtering) {
+			lastValue = newValue;
+			return newValue;
+		} else {
+			float gain;
+			float delta = Mathf.Abs (newValue - lastValue);
+
+			if (delta <= deltaLow) { // Low velocity
+				gain = gainLow;
+			} else if (delta >= deltaHigh) { // High velocity
+				gain = gainHigh;
+			} else { // Medium velocity
+				gain = gainHigh + ((delta - deltaHigh) / (deltaLow - deltaHigh)) * (gainLow - gainHigh);
+			}
+
+			float newFilteredValue = gain * newValue + (1 - gain) * (filteredValue + trend);
+			trend = trendGain * (newFilteredValue - filteredValue) + (1 - trendGain) * trend;
+
+			lastValue = newValue;
+			return newFilteredValue;
+		}
+	}
+}
---
+++ warpyworkspace/WarpyWorkspace/Assets/Plugins/AdaptiveDoubleExponentialFilterVector3.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class AdaptiveDoubleExponentialFilterVector3
+{
+	private AdaptiveDoubleExponentialFilterFloat x;
+	private AdaptiveDoubleExponentialFilterFloat y;
+	private AdaptiveDoubleExponentialFilterFloat z;
+
+	public Vector3 Value
+	{
+		get { return new Vector3(x.Value, y.Value, z.Value); }
+		set { Update(value); }
+	}
+
+	public AdaptiveDoubleExponentialFilterVector3()
+	{
+		x = new AdaptiveDoubleExponentialFilterFloat();
+		y = new AdaptiveDoubleExponentialFilterFloat();
+		z = new AdaptiveDoubleExponentialFilterFloat();
+	}
+
+	private void Update(Vector3 v)
+	{
+		x.Value = v.x;
+		y.Value = v.y;
+		z.Value = v.z;
+	}
+
+
+    private float deltaLow;
+    private float deltaHigh;
+    private float gainLow;
+    private float gainHigh;
+    private float trendGain;
+    public float DeltaLow
+    {
+        get { return deltaLow; }
+        set { deltaLow = value;
+            x.DeltaLow = value;
+            y.DeltaLow = value;
+            z.DeltaLow = value;
+        }
+    }
+
+    public float DeltaHigh
+    {
+        get { return deltaHigh; }
+        set { deltaHigh = value;
+            x.DeltaHigh = value;
+            y.DeltaHigh = value;
+            z.DeltaHigh = value;
+        }
+    }
+
+    public float GainLow
+    {
+        get { return gainLow; }
+        set { gainLow = value;
+            x.GainLow = value;
+            y.GainLow = value;
+            z.GainLow = value;
+        }
+    }
+
+    public float GainHigh
+    {
+        get { return gainHigh; }
+        set { gainHigh = value;
+            x.GainHigh = value;
+            y.GainHigh = value;
+            z.GainHigh = value;
+        }
+    }
+
+    public float TrendGain
+    {
+        get { return trendGain; }
+        set { trendGain = value;
+            x.TrendGain = value;
+            y.TrendGain = value;
+            z.TrendGain = value;
+        }
+    }
+
+}

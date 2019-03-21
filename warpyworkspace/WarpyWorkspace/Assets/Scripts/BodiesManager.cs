4d34957704f5edcc414febb7e9a7151eb0fd80c2
Mauricio Sousa <antonio.sousa@ist.utl.pt>
2019-03-21 19:43:05 +0000

    :| IK Scene

--- warpyworkspace/WarpyWorkspace/Assets/Scripts/BodiesManager.cs
+++ warpyworkspace/WarpyWorkspace/Assets/Scripts/BodiesManager.cs
@@ -14,13 +14,30 @@ public class BodiesManager : MonoBehaviour
 
     public Transform humanGO;
 
+    private List<Transform> _listOfChildren;
+    private Dictionary<BodyJointType, Transform> _bodyTrans;
+
+
     void Start()
     {
         _humans = new Dictionary<string, Human>();
+        _listOfChildren = new List<Transform>();
+        _bodyTrans = new Dictionary<BodyJointType, Transform>();
+        _populateBodyTrans(humanGO);
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            _disassembleHierarchy(humanGO);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            _assembleHumanHierarchy(humanGO);
+        }
+
         if (_humans.Count > 0)
         {
             if (human != null && _humanLocked && _humans.ContainsKey(human.id))
@@ -48,10 +65,12 @@ public class BodiesManager : MonoBehaviour
                 human = newHuman;
             }
 
+            _disassembleHierarchy(humanGO);
             foreach (BodyJointType joint in (BodyJointType[])Enum.GetValues(typeof(BodyJointType)))
             {
                 humanGO.Find(joint.ToString()).localPosition = human.body.Joints[joint];
             }
+            _assembleHumanHierarchy(humanGO);
         }
         _cleanDeadHumans();
     }
@@ -92,5 +111,97 @@ public class BodiesManager : MonoBehaviour
         }
 
         deadhumans = new List<Human>();
+    }
+
+    private void _getAllChildren(Transform trans)
+    {
+        if (trans.childCount > 0)
+        {
+            foreach (Transform t in trans)
+            {
+                if (t.name != "Sphere")
+                {
+                    _listOfChildren.Add(t);
+                    _getAllChildren(t);
+                }
+            }
+        }
+    }
+
+    public void _disassembleHierarchy(Transform trans)
+    {
+        _listOfChildren = new List<Transform>();
+        if (trans.childCount > 0)
+        {
+            foreach (Transform t in trans)
+            {
+                _getAllChildren(t);
+            }
+        }
+
+        foreach (Transform g in _listOfChildren)
+        {
+            g.parent = trans;
+        }
+
+        _listOfChildren.Add(trans.Find(BodyJointType.spineBase.ToString()));
+    }
+
+    private void _assembleHumanHierarchy(Transform trans)
+    {
+        _bodyTrans[BodyJointType.spineMid].parent = _bodyTrans[BodyJointType.spineBase];
+        _bodyTrans[BodyJointType.spineShoulder].parent = _bodyTrans[BodyJointType.spineMid];
+
+        _bodyTrans[BodyJointType.neck].parent = _bodyTrans[BodyJointType.spineShoulder];
+        _bodyTrans[BodyJointType.head].parent = _bodyTrans[BodyJointType.neck];
+
+        _bodyTrans[BodyJointType.rightShoulder].parent = _bodyTrans[BodyJointType.spineShoulder];
+        _bodyTrans[BodyJointType.rightElbow].parent = _bodyTrans[BodyJointType.rightShoulder];
+        _bodyTrans[BodyJointType.rightWrist].parent = _bodyTrans[BodyJointType.rightElbow];
+        _bodyTrans[BodyJointType.rightHand].parent = _bodyTrans[BodyJointType.rightWrist];
+        _bodyTrans[BodyJointType.rightThumb].parent = _bodyTrans[BodyJointType.rightWrist];
+        _bodyTrans[BodyJointType.rightHandTip].parent = _bodyTrans[BodyJointType.rightHand];
+
+        _bodyTrans[BodyJointType.leftShoulder].parent = _bodyTrans[BodyJointType.spineShoulder];
+        _bodyTrans[BodyJointType.leftElbow].parent = _bodyTrans[BodyJointType.leftShoulder];
+        _bodyTrans[BodyJointType.leftWrist].parent = _bodyTrans[BodyJointType.leftElbow];
+        _bodyTrans[BodyJointType.leftHand].parent = _bodyTrans[BodyJointType.leftWrist];
+        _bodyTrans[BodyJointType.leftThumb].parent = _bodyTrans[BodyJointType.leftWrist];
+        _bodyTrans[BodyJointType.leftHandTip].parent = _bodyTrans[BodyJointType.leftHand];
+
+        _bodyTrans[BodyJointType.rightHip].parent = _bodyTrans[BodyJointType.spineBase];
+        _bodyTrans[BodyJointType.rightKnee].parent = _bodyTrans[BodyJointType.rightHip];
+        _bodyTrans[BodyJointType.rightAnkle].parent = _bodyTrans[BodyJointType.rightKnee];
+        _bodyTrans[BodyJointType.rightFoot].parent = _bodyTrans[BodyJointType.rightAnkle];
+
+        _bodyTrans[BodyJointType.leftHip].parent = _bodyTrans[BodyJointType.spineBase];
+        _bodyTrans[BodyJointType.leftKnee].parent = _bodyTrans[BodyJointType.leftHip];
+        _bodyTrans[BodyJointType.leftAnkle].parent = _bodyTrans[BodyJointType.leftKnee];
+        _bodyTrans[BodyJointType.leftFoot].parent = _bodyTrans[BodyJointType.leftAnkle];
+    }
+
+    private void _populateBodyTrans(Transform humanGO)
+    {
+        _bodyTrans.Clear();
+        _disassembleHierarchy(humanGO);
+        foreach (BodyJointType joint in (BodyJointType[])Enum.GetValues(typeof(BodyJointType)))
+        {
+                _bodyTrans[joint] = _getTransformOfJoint(joint);
+        }
+        _assembleHumanHierarchy(humanGO);
+    }
+
+    private Transform _getTransformOfJoint(BodyJointType joint)
+    {
+        Transform ret = null;
+
+        foreach (Transform t in _listOfChildren)
+        {
+            if (t.gameObject.name == joint.ToString())
+            {
+                ret = t; break;
+            }
+        }
+        return ret;
     }
 }
---
+++ warpyworkspace/WarpyWorkspace/Assets/Scripts/IK/IKWarpInit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKWarpInit : MonoBehaviour {
+
+    public BodiesManager _bodies;
+    public UdpBodiesListener _bodiesListener;
+
+    public TrackerMesh _ravatar;
+
+
+    public int CT_trackerBroadcastPort;
+    public int RAV_trackerPort;
+    public int RAV_listenPort;
+
+
+    void Awake () {
+        Application.runInBackground = true;
+
+        _bodiesListener.startListening(CT_trackerBroadcastPort);
+
+        _ravatar.Init(RAV_trackerPort, RAV_listenPort, transform);
+    }
+
+    void Update () {
+		
+	}
+}

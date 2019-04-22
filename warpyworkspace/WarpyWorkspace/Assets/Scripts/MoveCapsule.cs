8eb8c4b646056a9e8e970c0ba462f8feb13fe142
rafaelkuffner <rafaelkuffner@gmail.com>
2019-04-22 17:59:17 +0100

    gbngfbnfgbndf

---
+++ warpyworkspace/WarpyWorkspace/Assets/Scripts/MoveCapsule.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCapsule : MonoBehaviour {
+
+    public Transform spineMid;
+
+	void Update () {
+        transform.position = spineMid.position;	
+	}
+}

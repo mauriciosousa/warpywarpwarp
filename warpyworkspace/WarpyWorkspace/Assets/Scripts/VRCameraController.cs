5388b74c456c34fe67ea7b18eb2de2fbed27bd6b
rafaelkuffner <rafaelkuffner@gmail.com>
2019-07-18 18:16:52 +0100

    cdcdc

--- warpyworkspace/WarpyWorkspace/Assets/Scripts/VRCameraController.cs
+++ warpyworkspace/WarpyWorkspace/Assets/Scripts/VRCameraController.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class VRCameraController : MonoBehaviour {
 
     public GameObject childGO;
+    public AlteredTelepresenceMain main;
 
+    [Space(5)]
     public Vector3 threshold;
+
 
     void LateUpdate()
     {
 
-        this.transform.localPosition = -childGO.transform.localPosition + threshold; /*new Vector3((-UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye).x),
+        this.transform.localPosition = -childGO.transform.localPosition + (main.setupLocation == SetupLocation.LEFT ? threshold : - threshold);
+        
+        /*new Vector3((-UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye).x),
                  (-UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye).y),
                  (-UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye).z));*/
 

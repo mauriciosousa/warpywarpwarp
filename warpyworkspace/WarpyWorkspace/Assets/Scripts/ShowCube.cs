471b1666ea66491bf2927bbf522932701a12092c
rafaelkuffner <rafaelkuffner@gmail.com>
2019-09-11 17:02:25 +0100

    new calib method

---
+++ warpyworkspace/WarpyWorkspace/Assets/Scripts/ShowCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCube : MonoBehaviour {
+
+    public AlteredTelepresenceMain main;
+
+	void Start () {
+
+        gameObject.active = main.calibrationMode;
+		
+	}	
+
+	void Update () {
+		
+	}
+}
